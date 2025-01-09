52e9130d9acf4f3d24f4fa4f4da27add506cb7e7
masulo1212 <masulo1212@gmail.com>
2025-01-09 22:03:35 +0800

    Fix Next.js config and GitHub Actions workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+      remotePatterns: []
+    },
   },
   basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '/',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
 };
 
 export default nextConfig;
