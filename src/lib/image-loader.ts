05e56d01e4b7cf8069b339e9e507768d9ebd6896
masulo1212 <masulo1212@gmail.com>
2025-01-14 12:07:24 +0800

    Updates

--- src/lib/image-loader.ts
+++
@@ -1,4 +0,0 @@
-export default function imageLoader({ src, width, quality }: { src: string; width: number; quality?: number }) {
-  const basePath = process.env.NODE_ENV === 'production' ? '/portfolio' : '';
-  return `${basePath}${src}${width ? `?w=${width}` : ''}${quality ? `&q=${quality}` : ''}`;
-}
